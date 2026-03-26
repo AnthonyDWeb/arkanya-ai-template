04a8664106d7f9a35dcf1905bc177d68569f69ec
Your Name <delforge.anthony@gmail.com>
2026-03-26 08:55:52 +0100

    add legal type again

--- lib/config/getLegal.ts
+++ lib/config/getLegal.ts
@@ -1,6 +1,7 @@
-import legal from "@/config/legal.json"
+import legalJson from "@/config/legal.json"
 import {LegalCongig} from "@/types/legal";
 
+const legal = legalJson as LegalCongig;
 export function getLegal(): LegalCongig {
     return legal
 }
--- lib/config/getSite.ts
+++ lib/config/getSite.ts
@@ -1,5 +1,7 @@
-import site from "@/config/site.json"
+import siteJson from "@/config/site.json"
 import {SiteConfig} from "@/types/site";
+
+const site = siteJson as SiteConfig;
 
 export function getSite(): SiteConfig {
     return site
