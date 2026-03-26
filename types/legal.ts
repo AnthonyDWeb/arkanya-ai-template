1d71f1461fc1ef10bda29b99590917183048735e
Your Name <delforge.anthony@gmail.com>
2026-03-26 09:01:33 +0100

    add legal type full

--- types/legal.ts
+++ types/legal.ts
@@ -1,7 +1,21 @@
 export type LegalCongig = {
-    companyName: string,
-    ownerName: string,
-    address: string,
-    email: string,
-    phone: string,
+    "companyName": string,
+    "ownerName": string,
+    "legalStatus": string,
+
+    "address": string,
+    "email": string,
+    "phone": string,
+
+    "siret": string,
+    "rcs": string,
+
+    "website": string,
+
+    "hostName": string,
+    "hostAddress": string,
+    "hostWebsite": string,
+
+    "dataController": string,
+    "dataEmail": string
 }
